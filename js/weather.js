9d0cdfe9ec9ce40b29e1579b05c5ade0b3391fce
Tom Lister <tom.lister@tomlister.local>
2016-08-20 09:01:27 +1000

    edited stuff

--- js/weather.js
+++ js/weather.js
@@ -1,30 +1,34 @@
+  function code2Emoji(code) {
+    if (code == '32') {
+            //Sunny
+           return '☀️';
+        } else if (code == '31') {
+            //Clear (Night)
+            return '🌃';
+        } else if (code == '26') {
+            //Cloudy
+            return '☁️';
+        } else if (code == '11' | code == '12') {
+            //Rain
+           return '🌧';
+        }
+  }
   var callbackFunction1 = function(data) {
     var oneword = data.query.results.channel.item.condition.text;
     var weathercode = data.query.results.channel.item.condition.code;
     var temp = Math.round((data.query.results.channel.item.condition.temp - 32) * 5 / 9);
     window.onload = function () {
     	document.getElementById('onewordweather').innerHTML = oneword;
-        if (weathercode == '32') {
-            //Sunny
-           document.getElementById('emoji-left').innerHTML = '<h2>'+'☀️'+'</h2>';
-        } else if (weathercode == '31') {
-            //Clear (Night)
-            document.getElementById('emoji-left').innerHTML = '<h2>'+'🌃'+'</h2>';
-        } else if (weathercode == '26') {
-            //Cloudy
-            document.getElementById('emoji-left').innerHTML = '<h2>'+'☁️'+'</h2>';
-        } else if (weathercode == '11' | weathercode == '12') {
-            //Rain
-            document.getElementById('emoji-left').innerHTML = '<h2>'+'🌧'+'</h2>';
-        }
+        document.getElementById('emoji-left').innerHTML = '<h2>'+code2Emoji(weathercode)+'</h2>';
     	document.getElementById('temp').innerHTML = temp + ' °C';
 
         //Hidden div
         
-        var forecast = data.query.results.channel.item.forecast
+        var forecast = data.query.results.channel.item.forecast;
         for (var i = 0; i < 5; i++) {
             var high = Math.round((forecast[i].high - 32) * 5 / 9);
-            $(".wet-hidden").append("<h2 style='font-weight: 400; margin: 0;' >On the "+forecast[i].date+" it will be "+high+" and "+forecast[i].text+"</h1>")
+            $(".wet-hidden").append('<div class="forecastemoji"><h2>'+code2Emoji(weathercode)+'</h2></div>');
+            $(".wet-hidden").append("<h2 style='font-weight: 200; margin: 0;' >"+forecast[i].date+", "+forecast[i].text+"</h2>");
         }    
     }
   };
